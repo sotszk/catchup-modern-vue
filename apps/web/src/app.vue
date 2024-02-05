1d2aef96a308dec4af952e89a1a106c78ddb69ce
Sota Suzuki <s.suzuki@ernie.co.jp>
2024-02-05 16:23:27 +0900

    update apps/web

--- apps/web/src/app.vue
+++ apps/web/src/app.vue
@@ -1,8 +1,49 @@
 <script setup lang="ts">
-import { Page } from "ui";
 import { name } from "../package.json";
 </script>
 
 <template>
-  <Page :app-name="name" />
+  <div class="container">
+    <span inline-block px4 py2 border border-solid border-white rounded-10>This package is apps/{{ name }}</span>
+  </div>
 </template>
+
+<style>
+:root {
+  --max-width: 1100px;
+  --font-mono: ui-monospace, Menlo, Monaco, "Cascadia Mono", "Segoe UI Mono",
+    "Roboto Mono", "Oxygen Mono", "Ubuntu Monospace", "Source Code Pro",
+    "Fira Mono", "Droid Sans Mono", "Courier New", monospace;
+
+  --foreground-rgb: 255, 255, 255;
+  --background-start-rgb: 0, 0, 0;
+  --background-end-rgb: 0, 0, 0;
+
+  --font-mono: ui-monospace, Menlo, Monaco, "Cascadia Mono", "Segoe UI Mono",
+    "Roboto Mono", "Oxygen Mono", "Ubuntu Monospace", "Source Code Pro",
+    "Fira Mono", "Droid Sans Mono", "Courier New", monospace;
+}
+
+html,
+body {
+  max-width: 100vw;
+  overflow-x: hidden;
+  font-family: "Inter", sans-serif;
+}
+
+body {
+  color: rgb(var(--foreground-rgb));
+  background: linear-gradient(
+      to bottom,
+      transparent,
+      rgb(var(--background-end-rgb))
+    )
+    rgb(var(--background-start-rgb));
+}
+</style>
+
+<style scoped>
+.container {
+  padding: 16px;
+}
+</style>
